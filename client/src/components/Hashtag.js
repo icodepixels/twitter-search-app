b0ce4c39c5b4d651f7dc3dbe6b37ad3f1af02352
Jason Roberts <jroberts@inboxlab.com>
2021-05-29 19:44:51 -0600

    check for lower case

--- client/src/components/Hashtag.js
+++ client/src/components/Hashtag.js
@@ -2,7 +2,7 @@ import "./Hashtag.scss";
 
 function Hashtag(props) {
     return (
-        <button key={props.title} onClick={() => props.handleSubmit(props.title)} className={`Hashtag__button ${props.className}`} disabled={props.filterIsOn && props.title !== props.selectedFilter}>#{props.title}</button>
+        <button key={props.title} onClick={() => props.handleSubmit(props.title)} className={`Hashtag__button ${props.className}`} disabled={props.filterIsOn && props.title.toLowerCase() !== props.selectedFilter.toLowerCase()}>#{props.title}</button>
     );
 }
 
